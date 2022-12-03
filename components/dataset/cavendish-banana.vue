98922c1edebabc751c6b2c97b9549a9986f61489
GHOME-PC <pongsagorn.ch@gmail.com>
2022-12-03 21:58:41 +0700

    fix_cavendish_page

--- components/dataset/cavendish-banana.vue
+++ components/dataset/cavendish-banana.vue
@@ -10,14 +10,14 @@
 
       <b-card-text>Bananas are rich in minerals and various nutrients. that the body should receive and provide up to 100 kcal of energy per unit. Because in bananas there are 3 types of sugar: sucrose, fructose, glucose, and dietary fiber. Thus our body will get energy immediately.  Two bananas can provide energy for up to 80-100 minutes (160-200Kcal) and also have other benefits such as stress relief, brain power, reduce the occurrence of ulcers in the stomach, reduce anemia reduction of the occurrence of cramps, nourishing the nervous system, etc.</b-card-text>
       <b-card-text>Our team built the Cavendish banana dataset while ripening. The dataset aim to (1) Banana Segmentation from background using DIP (2) DeepSemeticInstanceSegmentation (3) Ripeness Level Prediction for both DIP method and CNN Classification. We already labeled (annotation) for the data. The ripeness prediction level divied into 8 level.</b-card-text>
-      <b-card-text></b-card-text>
+      <b-card-text />
       <br>
     </b-card>
     <h3>To Download</h3>
-    <p>Please read and accept the Agreement and Disclaimer below before download. If you download this dataset, we count you already accepted the agreement and disclaimer.</p>
+    <p>Please read and accept the Agreement and Disclaimer below before download. <b>If you download this dataset, we count you already accepted the agreement and disclaimer.</b></p>
     <p>1. The dataset must be only used for non-commercial or educational purposes.</p>
     <p>2. No responsibility or any guarantee for any incidents, or damages caused by the direct or indirect usage of this dataset.</p>
-    <p>3. This dataset is copyright of <a href="https://www.rmutsv.ac.th" target="_blank">Rajamangala University of Technology Srivijaya</a>,for commercial usage please contact <a href="http://tlo.rmutsv.ac.th/tlo/">Technology Licensing Office</a></p>
+    <p>3. This dataset is copyright of <a href="https://www.rmutsv.ac.th" target="_blank">Rajamangala University of Technology Srivijaya</a>,for commercial usage please contact <a href="http://tlo.rmutsv.ac.th/tlo/">Technology Licensing Office</a>.</p>
     <p>4. If you used the dataset, please consider to referencing paper [Automatic Cavendish Banana Ripening using Digital Image Processing and Deep Learning].</p>
     <table>
       <tr>
@@ -67,7 +67,7 @@
       <tr>
         <td>Set of Images while Rippening</td>
         <td>
-          <b-button href="https://dataset.gezdev.com/dataset/CavendishBanana/WhileRipening_CompletedSet.zip" variant="success">
+          <b-button href="https://dataset.gezdev.com/CavendishBanana/WhileRipening_CompletedSet.zip" variant="success">
             Download
           </b-button>
         </td>
@@ -110,5 +110,9 @@ table {
           th {
           background-color: #4CAF50;
           color: white;
+          }
+          p{
+            padding-left: 35px;
+            padding-right: 10px;
           }
 </style>
